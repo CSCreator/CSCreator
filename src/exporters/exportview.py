c6a057b77ab17de7d302e807886a3960e7a49e67
CSCreator <charactersheetcreator@gmail.com>
2020-06-27 12:44:29 +0200

    Made pdf dialog generic to prevent duplication

--- src/exporters/exportview.py
+++ src/exporters/exportview.py
@@ -5,6 +5,8 @@ import os
 from PySide2.QtWidgets import QFileDialog, QDialog, QVBoxLayout, QFormLayout, QLabel, QComboBox, QDialogButtonBox
 from obsub import event
 
+from src.views.pdfdialog import PdfDialog
+
 logger = logging.getLogger(__name__)
 
 from PySide2.QtCore import (
@@ -13,86 +15,6 @@ from PySide2.QtCore import (
     Qt,
     QStandardPaths,
 )
-
-
-class ExportPdfDialog(QDialog):
-    def __init__(self, parent, importers_stringlist):
-        super(ExportPdfDialog, self).__init__(parent)
-        self.resize(320, 240)
-
-        self.importer_selected = 0
-
-        self.verticalLayout = QVBoxLayout(self)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.formLayout = QFormLayout()
-        self.formLayout.setObjectName("formLayout")
-        self.selected_file_label = QLabel(self)
-        self.selected_file_label.setObjectName("selected_file_label")
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.selected_file_label)
-
-        self.importer_label = QLabel(self)
-        self.importer_label.setObjectName("exporter_label")
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.importer_label)
-
-        self.importer_combo_box = QComboBox(self)
-        self.importer_combo_box.setObjectName("exporter_combo_box")
-        self.importer_combo_box.addItems(importers_stringlist)
-
-        def set_importer_selected(x):
-            self.importer_selected = x
-
-        self.importer_combo_box.currentIndexChanged.connect(set_importer_selected)
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.importer_combo_box)
-
-        self.selected_label = QLabel(self)
-        self.selected_label.setObjectName("selected_label")
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.selected_label)
-
-        self.importer_status = QLabel(self)
-        self.importer_status.setObjectName("exporter_status")
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.importer_status)
-
-        self.verticalLayout.addLayout(self.formLayout)
-
-        self.buttonBox = QDialogButtonBox(self)
-        self.buttonBox.setObjectName("buttonBox")
-        self.buttonBox.setOrientation(Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
-
-        self.verticalLayout.addWidget(self.buttonBox)
-
-        self.retranslateUi(self)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        QMetaObject.connectSlotsByName(self)
-
-    # setupUi
-
-    def retranslateUi(self, dialog):
-        dialog.setWindowTitle(
-            QCoreApplication.translate("dialog", "Export Character Sheet", None)
-        )
-        self.selected_file_label.setText(
-            QCoreApplication.translate("dialog", "File", None)
-        )
-        self.importer_label.setText(
-            QCoreApplication.translate("dialog", "Exporter", None)
-        )
-        self.selected_label.setText(
-            QCoreApplication.translate("dialog", "TextLabel", None)
-        )
-        self.importer_status.setText(
-            QCoreApplication.translate("dialog", "TextLabel", None)
-        )
-
-    # retranslateUi
-
 
 def do_file(parent):
     dir_to_open = QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
@@ -138,7 +60,9 @@ class ExportPdfWizardFactory:
         logger.info("Opening Export PDF Wizard")
         file_name = do_file(parent)
         exporters_names, exporters_files = find_exporters()
-        ui = ExportPdfDialog(parent, exporters_names)
+        ui = PdfDialog(parent, exporters_names)
+        ui.importer_label.setText("Exporter")
+        ui.setWindowTitle("Export Character Sheet")
         ui.selected_label.setText(file_name[0])
         status = ui.exec()
         selected_importer_file_index = exporters_files[ui.importer_selected]
--- src/importers/importview.py
+++ src/importers/importview.py
@@ -5,6 +5,8 @@ import os
 from PySide2.QtWidgets import QFileDialog, QDialog, QVBoxLayout, QFormLayout, QLabel, QComboBox, QDialogButtonBox
 from obsub import event
 
+from src.views.pdfdialog import PdfDialog
+
 logger = logging.getLogger(__name__)
 
 from PySide2.QtCore import (
@@ -13,86 +15,6 @@ from PySide2.QtCore import (
     Qt,
     QStandardPaths,
 )
-
-
-class ImportPdfDialog(QDialog):
-    def __init__(self, parent, importers_stringlist):
-        super(ImportPdfDialog, self).__init__(parent)
-        self.resize(320, 240)
-
-        self.importer_selected = 0
-
-        self.verticalLayout = QVBoxLayout(self)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.formLayout = QFormLayout()
-        self.formLayout.setObjectName("formLayout")
-        self.selected_file_label = QLabel(self)
-        self.selected_file_label.setObjectName("selected_file_label")
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.selected_file_label)
-
-        self.importer_label = QLabel(self)
-        self.importer_label.setObjectName("importer_label")
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.importer_label)
-
-        self.importer_combo_box = QComboBox(self)
-        self.importer_combo_box.setObjectName("importer_combo_box")
-        self.importer_combo_box.addItems(importers_stringlist)
-
-        def set_importer_selected(x):
-            self.importer_selected = x
-
-        self.importer_combo_box.currentIndexChanged.connect(set_importer_selected)
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.importer_combo_box)
-
-        self.selected_label = QLabel(self)
-        self.selected_label.setObjectName("selected_label")
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.selected_label)
-
-        self.importer_status = QLabel(self)
-        self.importer_status.setObjectName("importer_status")
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.importer_status)
-
-        self.verticalLayout.addLayout(self.formLayout)
-
-        self.buttonBox = QDialogButtonBox(self)
-        self.buttonBox.setObjectName("buttonBox")
-        self.buttonBox.setOrientation(Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
-
-        self.verticalLayout.addWidget(self.buttonBox)
-
-        self.retranslateUi(self)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        QMetaObject.connectSlotsByName(self)
-
-    # setupUi
-
-    def retranslateUi(self, dialog):
-        dialog.setWindowTitle(
-            QCoreApplication.translate("dialog", "Import Character Sheet", None)
-        )
-        self.selected_file_label.setText(
-            QCoreApplication.translate("dialog", "Selected File", None)
-        )
-        self.importer_label.setText(
-            QCoreApplication.translate("dialog", "Importer", None)
-        )
-        self.selected_label.setText(
-            QCoreApplication.translate("dialog", "TextLabel", None)
-        )
-        self.importer_status.setText(
-            QCoreApplication.translate("dialog", "TextLabel", None)
-        )
-
-    # retranslateUi
-
 
 def do_file(parent):
     dir_to_open = QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
@@ -138,7 +60,9 @@ class PdfWizardFactory:
         logger.info("Opening Import PDF Wizard")
         file_name = do_file(parent)
         importer_names, importer_files = find_importers()
-        ui = ImportPdfDialog(parent, importer_names)
+        ui = PdfDialog(parent, importer_names)
+        ui.importer_label.setText("Importer")
+        ui.setWindowTitle("Import Character Sheet")
         ui.selected_label.setText(file_name[0])
         status = ui.exec()
         selected_importer_file_index = importer_files[ui.importer_selected]
