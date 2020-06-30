dfd132788788912cac709eaf2e9bb49919f794b5
CSCreator <charactersheetcreator@gmail.com>
2020-06-30 15:57:56 +0200

    Generic button added

--- src/views/charactersubview.py
+++ src/views/charactersubview.py
@@ -1,7 +1,17 @@
-from PySide2.QtWidgets import QTableView, QWidget
-
+from PySide2.QtWidgets import QTableView, QWidget, QHBoxLayout, QPushButton, QVBoxLayout, QSpacerItem, QSizePolicy
 
 def get_view_for_submodel(submodel):
+    main_widget = QWidget()
+    hbox_layout = QHBoxLayout(main_widget)
+
     table_view = QTableView()
     table_view.setModel(submodel)
-    return table_view
+    hbox_layout.addWidget(table_view)
+
+    vbox_layout = QVBoxLayout()
+    add_button = QPushButton("+")
+    vbox_layout.addWidget(add_button)
+    vbox_layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
+    hbox_layout.addLayout(vbox_layout)
+
+    return main_widget
