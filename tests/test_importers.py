fc3e2e54a1d9675d80123cbd0234bf5f6c800a55
CSCreator <charactersheetcreator@gmail.com>
2020-07-04 12:10:26 +0200

    Test test

--- tests/test_importers.py
+++ tests/test_importers.py
@@ -2,17 +2,12 @@ import sys
 
 from PySide2.QtWidgets import QApplication
 
-from src.data.pluginmanager import PluginManager
+from src.data.pluginmanager import PluginManager, Plugin
 from src.importers.pdfimporter import PDFImporter
 
 
 def test_dnd_beyond_import() -> None:
-    app = QApplication(sys.argv)
-    app.setApplicationName("CSCreator")
-
-    plugin_manager = PluginManager()
-    plugin = plugin_manager.get_importer("DND Beyond")
-    importer = PDFImporter(plugin=plugin)
+    importer = PDFImporter(plugin=Plugin("src/data/importers/dndbeyond.json"))
     importer.load("pdfs/dndbeyond.pdf")
     player_controller = importer.player
     assert player_controller
