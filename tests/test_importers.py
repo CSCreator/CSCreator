1a30bcaa352b3789a64263c754471c41feea490b
CSCreator <charactersheetcreator@gmail.com>
2020-07-04 12:57:01 +0200

    Fixed memory exception

--- tests/test_importers.py
+++ tests/test_importers.py
@@ -1,10 +1,14 @@
+from PySide2.QtWidgets import QApplication
 
 from src.data.pluginmanager import Plugin
 from src.importers.pdfimporter import PDFImporter
 
 
 def test_dnd_beyond_import() -> None:
+    # If we not init a QApplication, the character_view writes to bad memory
+    app = QApplication()
     importer = PDFImporter(plugin=Plugin("src/data/importers/dndbeyond.json"))
-    importer.load("pdfs/dndbeyond.pdf")
+    print(importer)
+    importer.load("tests/pdfs/dndbeyond.pdf")
     player_controller = importer.player
     assert player_controller
